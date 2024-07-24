01e9594d04e748275a920095ef8f0b9794d6eef3
shikhap04 <the.shikha.patel@gmail.com>
2024-07-24 14:31:05 -0400

    error and success checking/messages

--- src/UserAccount.js
+++ src/UserAccount.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const UserAccount = () => {
   // Dummy data for user
+    const levels = ['Member', 'Employee', 'Administrator'];
+  
+
   const user = {
     name: sessionStorage.getItem('username'),
+    accountLevel: sessionStorage.getItem('accountLevel'),
+    title: levels[sessionStorage.getItem('accountLevel') - 1],
     borrowedBooks: [
       { id: 1, title: 'Book 1', dueDate: '2024-07-10' },
       { id: 2, title: 'Book 2', dueDate: '2024-07-15' },
@@ -13,6 +18,7 @@ const UserAccount = () => {
   return (
     <div>
       <h1>{user.name}'s Account</h1>
+      <p>Account Status: {user.title}</p>
       <h2>Borrowed Books</h2>
       {user.borrowedBooks.map(book => (
         <div key={book.id} className="card">
