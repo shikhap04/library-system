0ea14b2c0376b10a6ad6f6408eecbac777d80925
shikhap04 <the.shikha.patel@gmail.com>
2024-07-24 14:31:24 -0400

    see account status

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -2,62 +2,93 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './css/createAccount.css';
+import './generalFunctions'
+import { checkAlphaNumeric } from './generalFunctions';
 
 const CreateAccount = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [accountLevel, setAccountLevel] = useState(1);
     const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
 
     const navigate = useNavigate();
 
     // FOR FUTURE TO MAKE OPTION SO THAT ADMIN CAN CREATE EMPLOYEE, USER AND ETC.
     const loggedIn = sessionStorage.getItem('loggedIn');
-    const currAccountLevel = sessionStorage.getItem('accountLevel');
-    var isAdmin = false;
-    var isEmployee = false;
-    if (loggedIn && currAccountLevel == 3) {
-        isAdmin = true;
-        isEmployee = true;
-    } else if (loggedIn && currAccountLevel == 2) {
-        isEmployee = true;
-    }
-    var selectLevel = "Please log in once an account has been created!";
-    if (isAdmin) {
-        selectLevel = <select 
-                value={accountLevel}
-                onInput={(e) => setAccountLevel(Number(e.target.value))}>
-                <option value = "1">User</option>
-                <option value = "2">Employee</option>
-                <option value = "3">Administrator</option>
-            </select>;
-    } else if (isEmployee) {
-        selectLevel = <select 
-                value={accountLevel}
-                onInput={(e) => setAccountLevel(Number(e.target.value))}>
-                <option value = "1">User</option>
-                <option value = "2">Employee</option>
-            </select>;
-    } 
-
-  const handleCreateAccount = async(e) => {
-    e.preventDefault();
-    
-    try {
-        const response = await axios.post('http://localhost:3001/login/createAccount', { username, password, accountLevel});
-        if (response.status === 200) {
-            if (accountLevel == 1) navigate('/account');
-            else navigate('/account');
-        } else {
-            console.log('Username taken in:', username);
-            console.log('Password taken in:', password);
-            setError('Username and Password already exists')
+    if (loggedIn) {
+        const currAccountLevel = sessionStorage.getItem('accountLevel');
+        var isAdmin = false;
+        var isEmployee = false;
+        if (loggedIn && currAccountLevel == 3) {
+            isAdmin = true;
+            isEmployee = true;
+        } else if (loggedIn && currAccountLevel == 2) {
+            isEmployee = true;
         }
-    } catch (error) {
-        setError('Account already exists, try another username!');
-        console.log('ERROR in create acc', error);
+        if (isAdmin) {
+            var selectLevel = <select 
+                    value={accountLevel}
+                    onInput={(e) => setAccountLevel(Number(e.target.value))}>
+                    <option value = "1">User</option>
+                    <option value = "2">Employee</option>
+                    <option value = "3">Administrator</option>
+                </select>;
+        } else if (isEmployee) {
+            var selectLevel = <select 
+                    value={accountLevel}
+                    onInput={(e) => setAccountLevel(Number(e.target.value))}>
+                    <option value = "1">User</option>
+                    <option value = "2">Employee</option>
+                </select>;
+        } 
     }
-  }
+    
+
+    const handleCreateAccount = async(e) => {
+        e.preventDefault();
+        if (!username || !password) {
+            setError('Cannot have empty username or password. Please try again!')
+            setSuccess(null)
+            return;
+        }
+        if (!checkAlphaNumeric(username) || !checkAlphaNumeric(password)) {
+            setError('Usernames and passwords can only contain letters and numbers. Please try again!')
+            setSuccess(null)
+            return;
+        }
+        
+        try {
+            const response = await axios.post('http://localhost:3001/login/createAccount', { username, password, accountLevel});
+            if (response.status === 200) {
+                if (accountLevel == 1 && !loggedIn) {
+                    setSuccess('Created an account successfully, please click login below to redirect to log in page')
+                    setError(null)
+                }
+                else {
+                    const levels = ['member', 'employee', 'administrator'];
+                    const successMessage = 'Created an ' + levels[accountLevel - 1] + ' account successfully!';
+                    setSuccess(successMessage);
+                    setError(null)
+                };
+            } else {
+                console.log('Username taken in:', username);
+                console.log('Password taken in:', password);
+                setError('Username and Password already exists')
+                setSuccess(null)
+            }
+        } catch (error) {
+            setError('Account already exists, try another username!');
+            setSuccess(null)
+            console.log('ERROR in create acc', error);
+        }
+    }
+
+    const handleRedirect = () => {
+        navigate('/logIn');
+    }
+
+
 
   return (
     <div>
@@ -71,10 +102,16 @@ const CreateAccount = () => {
                 <label>Password:</label>
                 <input type='text' value = {password} onChange={(e) => setPassword(e.target.value)}/>
             </div>
-            {isEmployee && (selectLevel)}
+            {loggedIn && isEmployee && (selectLevel)}
             <button type="submit">Create Account</button>
         </form>
         {error && <p style={{ color: 'red'}}>Error: {error}</p>}
+        {success && 
+            <div> 
+                <p style={{ color: 'green'}}>Success: {success}</p>
+                {!loggedIn && <button onClick={handleRedirect}>Redirect to logIn Page</button>}
+            </div>
+        }
     </div>
   );
 }
